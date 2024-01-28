5ad9d84f5817b2bfe1893776a5cc05cd9f89aae9
kevinle350 <74153829+kevinle350@users.noreply.github.com>
2024-01-28 01:18:18 -0800

    all files done

---
+++ serve.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+import subprocess
+import socket
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST', 'GET'])
+def server():
+    if request.method == 'POST':
+        subprocess.Popen(['python', 'stress_cpu.py'])
+        return 'CPU stress script started', 200
+    else:
+        hostname = socket.gethostname()    
+        IPAddr = socket.gethostbyname(hostname)    
+        return IPAddr
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", debug=True, port=5000)
